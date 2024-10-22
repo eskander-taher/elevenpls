969af8b4c8289e9e11ee7e2ab9807d40ae5ed499
eskander-taher <eskander.alshaibany@gmail.com>
2024-10-22 23:44:27 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ export default function Home() {
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<Image
 				className="dark:invert"
-				src="/next.svg"
-				alt="Next.js logo"
+				src="/elevenpls.svg"
+				alt="Elevenpls logo"
 				width={180}
 				height={38}
 				priority
 			/>
+			<h1>Soon</h1>
 		</div>
   );
 }
