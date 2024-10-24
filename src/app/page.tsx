3a645ecfe3e4d6b430d577eca7512b58ef67e9b5
eskander-taher <eskander.alshaibany@gmail.com>
2024-10-24 16:27:13 +0300

    add lamp light

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { BackgroundGradientAnimation } from "@/components/ui/background";
+import { LampDemo } from "@/components/ui/lamp";
 
 export default function Home() {
 	return (
 		<div>
-			<BackgroundGradientAnimation className="flex flex-row min-h-96 justify-center items-center">
-				<Image src="/elevenpls.svg" alt="Elevenpls logo" width={360} height={38} priority />
+			<BackgroundGradientAnimation>
+				<LampDemo />
 			</BackgroundGradientAnimation>
 		</div>
 	);
