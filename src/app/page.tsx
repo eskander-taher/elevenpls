46cad896e3e3001b6de954df21042852a684292f
eskander-taher <eskander.alshaibany@gmail.com>
2024-10-24 16:06:45 +0300

    add background animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import Image from "next/image";
+import { BackgroundGradientAnimation } from "@/components/ui/background";
 
 export default function Home() {
-  return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<Image
-				className="dark:invert"
-				src="/elevenpls.svg"
-				alt="Elevenpls logo"
-				width={180}
-				height={38}
-				priority
-			/>
-			<h1>Soon</h1>
+	return (
+		<div>
+			<BackgroundGradientAnimation className="flex flex-row min-h-96 justify-center items-center">
+				<Image src="/elevenpls.svg" alt="Elevenpls logo" width={360} height={38} priority />
+			</BackgroundGradientAnimation>
 		</div>
-  );
+	);
 }
