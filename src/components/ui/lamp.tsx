eca6b6dc01c81fb65904b760e47b615337fbb8fd
eskander-taher <eskander.alshaibany@gmail.com>
2024-10-24 17:23:06 +0300

    add macbook

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -1,27 +1,10 @@
-"use client";
-import Image from "next/image";
+
+
 import React from "react";
 import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
-export function LampDemo() {
-	return (
-		<LampContainer>
-			<motion.h1
-				initial={{ opacity: 0.5, y: 100 }}
-				whileInView={{ opacity: 1, y: 0 }}
-				transition={{
-					delay: 0.3,
-					duration: 0.8,
-					ease: "easeInOut",
-				}}
-				className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-			>
-				<Image src="/elevenpls.svg" alt="Elevenpls logo" width={360} height={38} priority />
-			</motion.h1>
-		</LampContainer>
-	);
-}
+
 
 export const LampContainer = ({
 	children,
@@ -37,17 +20,17 @@ export const LampContainer = ({
 				className
 			)}
 		>
-			<div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
+			<div className="-translate-y-28 relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0 ">
 				<motion.div
 					initial={{ opacity: 0.5, width: "15rem" }}
 					whileInView={{ opacity: 1, width: "30rem" }}
 					transition={{
-						delay: 0.3,
+                        delay: 0.3,
 						duration: 0.8,
 						ease: "easeInOut",
 					}}
 					style={{
-						backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+                        backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
 					}}
 					className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
 				>
@@ -58,12 +41,12 @@ export const LampContainer = ({
 					initial={{ opacity: 0.5, width: "15rem" }}
 					whileInView={{ opacity: 1, width: "30rem" }}
 					transition={{
-						delay: 0.3,
+                        delay: 0.3,
 						duration: 0.8,
 						ease: "easeInOut",
 					}}
 					style={{
-						backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+                        backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
 					}}
 					className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 text-white [--conic-position:from_290deg_at_center_top]"
 				>
@@ -77,7 +60,7 @@ export const LampContainer = ({
 					initial={{ width: "8rem" }}
 					whileInView={{ width: "16rem" }}
 					transition={{
-						delay: 0.3,
+                        delay: 0.3,
 						duration: 0.8,
 						ease: "easeInOut",
 					}}
@@ -87,7 +70,7 @@ export const LampContainer = ({
 					initial={{ width: "15rem" }}
 					whileInView={{ width: "30rem" }}
 					transition={{
-						delay: 0.3,
+                        delay: 0.3,
 						duration: 0.8,
 						ease: "easeInOut",
 					}}
@@ -97,9 +80,10 @@ export const LampContainer = ({
 				<div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
 			</div>
 
-			<div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+			<div className="relative z-50 flex -translate-y-[30rem] flex-col items-center px-5">
 				{children}
 			</div>
 		</div>
 	);
 };
+
