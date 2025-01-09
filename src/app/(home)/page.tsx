4c38dd632a72c540b8acddc0266eb96369b26340
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-09 10:36:43 +0300

    make creative section responsive

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="flex justify-center heroBackgroundImage">
-			<div className="flex items-end">
-				<Image className="hidden xl:block bottom-0 left-10" src="/06.png" width={300} height={0} alt="hand" />
+			<div className="flex items-end overflow-hidden">
+				<Image className="relative hidden xl:block -bottom-20" src="/06.png" width={300} height={0} alt="hand" />
 				<CreativeServices />
 			</div>
 		</div>
