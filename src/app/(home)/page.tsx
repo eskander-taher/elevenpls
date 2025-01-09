c32969b255871b1ad1e6a20f66a044b3cc84617f
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-09 10:14:09 +0300

    add creative services section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex justify-center">
+		<div className="flex justify-center heroBackgroundImage">
 			<div className="h-screen flex items-end">
 				<Image className="hidden xl:block bottom-0 left-10" src="/06.png" width={300} height={0} alt="hand" />
 				<CreativeServices />
