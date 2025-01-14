284b89f6b722a08f9c0a2b28f460b67b8746a5cb
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-14 10:42:19 +0300

    add website under maintanance

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -7,17 +7,18 @@ export default function BackgroundBeamsDemo() {
 	return (
 		<div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
 			<div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-end">
-                <Image src="/logo_with_bg.png" width={100} height={100} alt="logo"/>
-				<h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 pb-6 to-neutral-600  text-center font-sans font-bold">
-					إنضم لقائمة الإنتظار
-				</h1>
+				<div className="flex gap-5">
+					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" />
+					<h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 pb-6 to-neutral-600  text-center font-sans font-bold">
+						eleven+
+					</h1>
+				</div>
 				<p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
 					الموقع تحت الصيانة في الوقت الحالي, يمكنكم التواصل عبر الايميل في الاسفل
 				</p>
 				<p className="text-center rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700">
 					info@elevenpls.com
 				</p>
-				
 			</div>
 			<BackgroundBeams />
 		</div>
