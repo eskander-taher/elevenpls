6302e913fe6c6f7f360394d4160860898adfbb02
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-14 15:12:01 +0300

    center logo

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -9,7 +9,7 @@ export default function BackgroundBeamsDemo() {
 			<div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center">
 				<div className="flex gap-5 justify-center items-center ">
 					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" className=" z-50"/>
-					<h1 className="text-7xl  text-white h-fit font-bold ">
+					<h1 className="text-7xl  text-white h-fit font-bold z-10">
 						eleven+
 					</h1>
 				</div>
--- src/components/water-drop.tsx
+++ src/components/water-drop.tsx
@@ -9,7 +9,7 @@ const WaterDropGrid = () => {
 			<div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center">
 				<div className="flex gap-5 justify-center items-center ">
 					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" className=" z-50" />
-					<h1 className="text-7xl  text-white h-fit font-bold ">eleven+</h1>
+					<h1 className="text-7xl  text-white h-fit font-bold z-10">eleven+</h1>
 				</div>
 				<p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
 					The website is currently under maintenance. You can contact us via the email.
