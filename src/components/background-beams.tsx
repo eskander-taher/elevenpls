a78e1492d462b3832b07a3974057cd4f7d25939d
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-14 13:21:23 +0300

    make hero responsive

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -7,9 +7,9 @@ export default function BackgroundBeamsDemo() {
 	return (
 		<div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
 			<div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-end">
-				<div className="flex gap-5">
+				<div className="flex gap-5 justify-center">
 					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" />
-					<h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 pb-6 to-neutral-600  text-center font-sans font-bold">
+					<h1 className="relative z-10 text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 pb-6 to-neutral-600  text-center font-sans font-bold">
 						eleven+
 					</h1>
 				</div>
