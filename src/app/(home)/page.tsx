9be92b9e4478ff5df9a9fb21680111a76f231695
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-14 13:34:01 +0300

    hide services secdtion

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 			<MotionGraphics />
 			<ContentDevelopment /> */}
 			<BackgroundBeams />
-			<CreativeServicesSection />
+			{/* <CreativeServicesSection /> */}
 		</>
 	);
 }
