0189294a71ce2e61d5522c77853ff73128392549
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-14 14:30:22 +0300

    change eleven to white

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -8,7 +8,7 @@ export default function BackgroundBeamsDemo() {
 		<div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
 			<div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center">
 				<div className="flex gap-5 justify-center">
-					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" />
+					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" className=" z-50"/>
 					<h1 className="relative z-10 text-7xl text-white pb-6 text-center font-bold">
 						eleven+
 					</h1>
