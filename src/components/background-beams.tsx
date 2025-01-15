539cca758276b29935963ace8f4c6de77eb54208
eskander-taher <eskander.alshaibany@gmail.com>
2025-01-15 21:18:41 +0300

    remove elevent text

--- src/components/background-beams.tsx
+++ src/components/background-beams.tsx
@@ -9,12 +9,10 @@ export default function BackgroundBeamsDemo() {
 			<div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center">
 				<div className="flex gap-5 justify-center items-center ">
 					<Image src="/logo_with_bg.png" width={100} height={100} alt="logo" className=" z-50"/>
-					<h1 className="text-7xl  text-white h-fit font-bold z-10">
-						eleven+
-					</h1>
+					
 				</div>
 				<p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-					The website is currently under maintenance. You can contact us via the email.
+					The website is currently under maintenance. You can contact us via email.
 				</p>
 				<p dir="rtl" className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
 					الموقع تحت الصيانة في الوقت الحالي, يمكنكم التواصل عبر الايميل.
